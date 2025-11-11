a750a2a416580706a51b6076f5b2f11f0ab688fa
ovictorqueiroz <victorbr442@gmail.com>
2025-11-11 18:56:27 -0300

    criando estrutura das pastas

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Atividade | 11/11/25</title>
 </head>
+
 <body>
-    
+    <h1>Login</h1>
+    <form action="" method="post">
+        <label for="user">Usuário</label>
+        <input type="email" name="user" id="user">
+        
+        <label for="pass">Senha</label>
+        <input type="password" name="pass" id="pass">
+
+        <input type="submit">
+    </form>
 </body>
 </html>
